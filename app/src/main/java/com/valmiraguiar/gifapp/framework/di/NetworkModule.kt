fc3eda6665102ae1631f2e1aa4c6f7cf49ba2e4c
Valmir Aguiar <valmiir.aguiar7@gmail.com>
2026-04-06 08:56:21 -0300

    feat: create network layer di

---
+++ app/src/main/java/com/valmiraguiar/gifapp/framework/di/NetworkModule.kt
@@ -0,0 +1,63 @@
+package com.valmiraguiar.gifapp.framework.di
+
+import com.valmiraguiar.core.data.constants.NetworkConstants.CONNECT_TIMEOUT_SECONDS
+import com.valmiraguiar.core.data.constants.NetworkConstants.READ_TIMEOUT_SECONDS
+import com.valmiraguiar.gifapp.BuildConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            setLevel(
+                if (BuildConfig.DEBUG) {
+                    HttpLoggingInterceptor.Level.BODY
+                } else {
+                    HttpLoggingInterceptor.Level.NONE
+                }
+
+            )
+        }
+    }
+
+    @Provides
+    fun provideOkHttpClient(
+        loggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .readTimeout(READ_TIMEOUT_SECONDS, TimeUnit.SECONDS)
+            .connectTimeout(CONNECT_TIMEOUT_SECONDS, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    fun provideGson(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        converterFactory: GsonConverterFactory
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(converterFactory)
+            .build()
+    }
+}
---
+++ core/src/main/java/com/valmiraguiar/core/data/constants/NetworkConstants.kt
@@ -0,0 +1,6 @@
+package com.valmiraguiar.core.data.constants
+
+object NetworkConstants {
+    const val CONNECT_TIMEOUT_SECONDS = 30L
+    const val READ_TIMEOUT_SECONDS = 30L
+}
